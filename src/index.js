796910c9063cc3bee639a1157c99ed02b0c8fc74
Minimise <jackson.whiteau@gmail.com>
2022-08-15 13:38:02 +0800

    Install firebase

--- src/index.js
+++ src/index.js
@@ -3,6 +3,38 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// Firebase
+
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyC1-AFb1xByzO4N4HKt5MIdMjs1BrKivno",
+
+  authDomain: "fakeit-d9bac.firebaseapp.com",
+
+  projectId: "fakeit-d9bac",
+
+  storageBucket: "fakeit-d9bac.appspot.com",
+
+  messagingSenderId: "642696077907",
+
+  appId: "1:642696077907:web:8ab7a5f3e681a686bc5ca6"
+
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
